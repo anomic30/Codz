ce142e973f599cf26c95f62074dfc18cf739502d
Anom Chakravorty <anomchakravorty3008@gmail.com>
2023-02-17 19:30:15 +0530

    landing page

---
+++ client/src/pages/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Landing.scss'
+
+const Landing = () => {
+  return (
+    <div className='Landing'>
+      <nav>
+          hi
+      </nav>
+    </div>
+  )
+}
+
+export default Landing
