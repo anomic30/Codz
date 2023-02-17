586af2ecd26d3bf99630a60f09e4b19cd3cc6fa7
adi <aditya4161@gmail.com>
2023-02-17 23:20:40 +0530

    added navbar

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './Landing.scss'
+import fullLogo from '../../assets/codz-full-logo.svg'
 
 const Landing = () => {
+  const navigate = useNavigate()
   return (
-    <div className='Landing'>
-      <nav>
-          hi
+    <div className="Landing">
+
+      <nav className="navbar">
+        <div className="navbar-content">
+          <img src={fullLogo} alt="logo" />
+          <div className="nav-links">
+            <div>Pricing</div>
+            <div>Feature</div>
+            <div className="nav-btn">Use Now</div>
+          </div>
+        </div>
       </nav>
+      <section className='hero'>
+
+      </section>
+      <section>
+
+      </section>
     </div>
   )
 }
