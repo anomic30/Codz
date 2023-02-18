d86fa3a4b0edd4eda3cb0e51c1519534420c7f7a
adi <aditya4161@gmail.com>
2023-02-18 18:29:57 +0530

    hero section added

--- client/src/pages/landing/Landing.jsx
+++ client/src/pages/landing/Landing.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Landing.scss'
-import fullLogo from '../../assets/codz-full-logo.svg'
+import fullLogo from '../../assets/icons/codz-full-logo.svg'
+import dmatter from '../../assets/images/darkmater.gif'
+import coding from '../../assets/images/coding.svg'
+import next_gen from '../../assets/images/next-gen.svg'
+import openai from '../../assets/images/openai.png'
 
 const Landing = () => {
   const navigate = useNavigate()
@@ -12,18 +16,53 @@ const Landing = () => {
         <div className="navbar-content">
           <img src={fullLogo} alt="logo" />
           <div className="nav-links">
-            <div>Pricing</div>
-            <div>Feature</div>
-            <div className="nav-btn">Use Now</div>
+            <div><a href=""> Pricing</a></div>
+            <div><a href="">Feature</a></div>
+            <a href=""><div className="nav-btn">Use Now</div></a>
           </div>
         </div>
       </nav>
-      <section className='hero'>
 
+      <section className="hero">
+        <div className="hero-content">
+          <div className="title">
+            <h1>
+              THE
+              FUTURE.
+            </h1>
+          </div>
+          <div className="sub-title">
+            <p>
+              The ultimate coding platform that lets you unleash the full potential of your coding skills with the power of our cutting edge Artificial Intelligence
+            </p>
+          </div>
+        </div>
       </section>
-      <section>
 
+      <section className="hero-graphics">
+        <img src={next_gen} alt="Next-Gen" />
+        <img  src={dmatter} alt="Dark Matter" />
+        <img  src={coding} alt="Coding" />
       </section>
+
+      <section className="intro">
+        <div className="intro-content">
+          <div className="intro-desc">
+            <p className="intro-title">Introducing <span id="gradient">Codz</span></p>
+            <p className="intro-sub-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure voluptate veniam quibusdam quod! Consectetur nostrum reiciendis neque quasi amet possimus quia ut officia, hic dolor necessitatibus cum ab perferendis blanditiis?</p>
+          </div>
+          <div className="intro-graphic">
+            <img src={openai} alt="OpenAI" />
+          </div>
+        </div>
+      </section>
+
+      <div className="playground">
+        <div className="playground-content">
+
+        </div>
+
+      </div>
     </div>
   )
 }
