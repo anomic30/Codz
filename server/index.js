a620ce830518c23cb45450103d23b5fd7d81c975
Anom Chakravorty <anomchakravorty3008@gmail.com>
2023-03-14 02:53:59 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // Define routes below
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello from Codz Server!");
 });
 app.use("/api", routes);
